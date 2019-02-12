de691964b070b8fbb71fa9a240456f709380ade1
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 22:41:59 +0300

    [feat: #] implement abstract methods

--- base_graph/__init__.py
+++ base_graph/__init__.py
@@ -5,6 +5,32 @@
 #
 ########################################################
 
-
 import abc
 import numpy as np
+
+
+class Graph(abc.ABC):
+
+    def __init__(self, numVertices, directed=False):
+        self.numVertices = numVertices
+        self.directed = directed
+
+    @abc.abstractMethod
+    def add_edge(self, v1, v2, weight):
+        pass
+
+    @abc.abstractMethod
+    def get_adjacent_vertices(self, v):
+        pass
+
+    @abc.abstractMethod
+    def get_indegree(self, v):
+        pass
+
+    @abc.abstractMethod
+    def get_edge_weight(self, v1, v2):
+        pass
+
+    @abc.abstractMethod
+    def display(self):
+        pass
