4c7526e27d06eb27a6830d49c5315295cb32507d
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 22:33:59 +0300

    [feat: #] add base class

---
+++ base_graph/__init__.py
@@ -0,0 +1,10 @@
+########################################################
+#
+# base class representation of a Graph with allt the
+# interface methods
+#
+########################################################
+
+
+import abc
+import numpy as np
