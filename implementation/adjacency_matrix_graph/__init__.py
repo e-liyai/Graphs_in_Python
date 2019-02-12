ead8aa9690d3c66d4ee8a5830260491af501051b
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 22:49:12 +0300

    [feat: #] restructure package

--- implementation/adjacency_matrix_graph/__init__.py
+++ implementation/adjacency_matrix_graph/__init__.py
@@ -3,6 +3,7 @@
 # Representation of a Graph for adjacency metrix
 #
 ########################################################
+from implementation.base_graph import Graph
 
 
 class AdjacencyMatrixGraph(Graph):
