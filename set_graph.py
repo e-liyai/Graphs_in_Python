4bc29482c9b4c3afc9f0a4ad9230171952f0a596
Andela-eugene <eugene.liyai@andela.com>
2019-02-13 19:18:24 +0300

    [feat: #] implement adjacency set example

---
+++ set_graph.py
@@ -0,0 +1,20 @@
+from implementation.adjacency_set_graph import AdjacencySetGraph
+
+graph = AdjacencySetGraph(4, directed=True)
+
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(2, 3)
+
+for i in range(4):
+    print('Adjacent to: ', i, graph.get_adjacent_vertices(i))
+
+for i in range(4):
+    print('Indegree: ', i, graph.get_indegree(i))
+
+for i in range(4):
+    for j in graph.get_adjacent_vertices(i):
+        print('Edge weight: ', i, ' ', j, ' weight: ',
+              graph.get_edge_weight(i, j))
+
+graph.display()
