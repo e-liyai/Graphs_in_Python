b13baecc8cfadb5c57df2cb605facaaad9de43e9
Andela-eugene <eugene.liyai@andela.com>
2019-02-13 19:06:29 +0300

    [feat: #] add in degree

--- implementation/adjacency_set_graph/__init__.py
+++ implementation/adjacency_set_graph/__init__.py
@@ -13,9 +13,34 @@ class AdjacencySetGraph(Graph):
         for i in range(numVertices):
             self.vertex_list.append(Node(i))
 
-        def add_edge(self, v1, v2, weight=1):
+    def add_edge(self, v1, v2, weight=1):
         if v1 >= self.numVertices or v2 >= self.numVertices or v1 < 0 or v2 < 0:
             raise ValueError('Vertices %d and %d are out of bounds' % (v1, v2))
 
         if weight != 1:
             raise ValueError('An adjacency set cannot rep edge weight != 1')
+
+        self.vertex_list[v1].add_edge(v2)
+
+        if self.directed == False:
+            self.vertex_list[v2].add_edge(v1)
+
+    def get_adjacent_vertices(self, v):
+
+        self.check_valid_vertex(v)
+
+        return self.vertex_list[v].get_adjacent_vertices()
+
+    def get_indegree(self, v):
+        self.check_valid_vertex(v)
+
+        indegree = 0
+        for i in range(self.numVertices):
+            if v in self.get_adjacent_vertices(i):
+                indegree += 1
+
+        return indegree
+
+    def check_valid_vertex(self, v):
+        if v < 0 or v >= self.numVertices:
+            raise ValueError('Cannot access vertex %d' % v)
