16e5755116f1f1a08508eb371e37b561d720e427
Andela-eugene <eugene.liyai@andela.com>
2019-02-17 15:16:24 +0300

    [feat: #] implement dikstras shortest path test

--- graph_shortest_path.py
+++ graph_shortest_path.py
@@ -1,5 +1,5 @@
 from implementation.adjacency_set_graph import AdjacencySetGraph
-from implementation.shortest_path import shortest_path
+from implementation.shortest_path import shortest_path, dikstras_shortest_path
 
 graph = AdjacencySetGraph(8, directed=True)
 
@@ -17,3 +17,20 @@ graph.add_edge(0, 7)
 shortest_path(graph, 0, 5)
 shortest_path(graph, 0, 6)
 shortest_path(graph, 7, 4)
+
+graph_two = AdjacencySetGraph(8, directed=False)
+
+graph_two.add_edge(0, 1, 1)
+graph_two.add_edge(1, 2, 2)
+graph_two.add_edge(1, 3, 6)
+graph_two.add_edge(2, 3, 2)
+graph_two.add_edge(1, 4, 3)
+graph_two.add_edge(3, 5, 1)
+graph_two.add_edge(5, 4, 5)
+graph_two.add_edge(3, 6, 1)
+graph_two.add_edge(6, 7, 1)
+graph_two.add_edge(0, 7, 8)
+
+dikstras_shortest_path(graph_two, 0, 6)
+dikstras_shortest_path(graph_two, 4, 7)
+dikstras_shortest_path(graph_two, 7, 0)
